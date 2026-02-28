90588a7486b8871a5b90cf5ff97c6354f435c4f4
cyber-uday04 <uday04122006@gmail.com>
2026-02-28 23:04:05 +0530

    Major upgrade: login, IST time, file upload, proper user tracking

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from flask_socketio import SocketIO, emit
 import os
 from datetime import datetime
+import pytz
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
-online_users = set()
+online_users = {}
 
 @app.route('/')
 def index():
@@ -15,25 +16,23 @@ def index():
 
 @socketio.on('join')
 def handle_join(username):
-    online_users.add(username)
-    emit('user_list', list(online_users), broadcast=True)
+    online_users[session.sid] = username
+    emit('user_list', list(online_users.values()), broadcast=True)
     emit('system_message', f"{username} joined the chat", broadcast=True)
 
 @socketio.on('send_message')
 def handle_message(data):
-    data['time'] = datetime.now().strftime("%H:%M")
+    india = pytz.timezone("Asia/Kolkata")
+    current_time = datetime.now(india).strftime("%I:%M %p")
+    data['time'] = current_time
     emit('receive_message', data, broadcast=True)
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    # username will be handled from frontend removal
-    pass
-
-@socketio.on('leave')
-def handle_leave(username):
-    if username in online_users:
-        online_users.remove(username)
-        emit('user_list', list(online_users), broadcast=True)
+    sid = session.sid
+    if sid in online_users:
+        username = online_users.pop(sid)
+        emit('user_list', list(online_users.values()), broadcast=True)
         emit('system_message', f"{username} left the chat", broadcast=True)
 
 if __name__ == '__main__':
