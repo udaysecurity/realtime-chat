d41dd34eb5e9fd2f4460ce7be30ffd8c53e3dbb8
cyber-uday04 <uday04122006@gmail.com>
2026-02-28 22:16:25 +0530

    Added online users, join/leave, timestamps

--- app.py
+++ app.py
@@ -1,18 +1,40 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, emit
 import os
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+
+online_users = set()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@socketio.on('message')
-def handle_message(msg):
-    send(msg, broadcast=True)
+@socketio.on('join')
+def handle_join(username):
+    online_users.add(username)
+    emit('user_list', list(online_users), broadcast=True)
+    emit('system_message', f"{username} joined the chat", broadcast=True)
+
+@socketio.on('send_message')
+def handle_message(data):
+    data['time'] = datetime.now().strftime("%H:%M")
+    emit('receive_message', data, broadcast=True)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    # username will be handled from frontend removal
+    pass
+
+@socketio.on('leave')
+def handle_leave(username):
+    if username in online_users:
+        online_users.remove(username)
+        emit('user_list', list(online_users), broadcast=True)
+        emit('system_message', f"{username} left the chat", broadcast=True)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
